2994e42e5741943fda07ef0e93a1c5bf11d1cac1
Murilo <murilobontorin2005@gmail.com>
2025-09-11 12:42:36 -0300

    Client: Recebimento feito (falta juntar). Server: Iniciada lógica de reenvio

--- client.py
+++ client.py
@@ -8,9 +8,9 @@ Requisitos do Cliente UDP:
     Simulação de Perda:
     TODO    Implementar uma opção (ex: via entrada do usuário ou configuração) que permita ao cliente descartar intencionalmente alguns segmentos recebidos do servidor. Isso é crucial para testar o mecanismo de recuperação de dados. A interface deve informar quais segmentos (ex: por número de sequência) estão sendo descartados.
     Recepção e Montagem:
-    TODO    Receber os segmentos do arquivo enviados pelo servidor.
+    FEITO    Receber os segmentos do arquivo enviados pelo servidor.
     TODO    Armazenar e ordenar os segmentos recebidos corretamente.
-    TODO    Verificar a integridade de cada segmento (ex: usando checksum ou resumos criptográficos como o MD5 e SHA.).
+    FEITO    Verificar a integridade de cada segmento (ex: usando checksum ou resumos criptográficos como o MD5 e SHA.).
     Verificação e Finalização:
     TODO    Após receber todos os segmentos esperados (ou um sinal de fim de transmissão do servidor), verificar a integridade e completude do arquivo.
     TODO    Se o arquivo estiver OK: Salvar o arquivo reconstruído localmente e informar o sucesso ao usuário. Opcionalmente, apresentar/abrir o arquivo.
@@ -24,12 +24,17 @@ Requisitos do Cliente UDP:
 """
 
 import socket
+import zlib
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
 #Cria o objeto de socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+
+def checksum_crc32(segmento):
+    return zlib.crc32(segmento) & 0xffffffff
 
 #Loop pra pegar pedir o ip mais de uma vez se precisar
 while True:
@@ -39,17 +44,41 @@ while True:
     try:
         udp_ip,udp_port = conexao.split(":")
         udp_port = int(udp_port)
+        break
 
     except ValueError:
         print("Formato inválido, insira no formato IP:PORTA")
         continue
 
-#Loop pra simular a conexão com o servidor
+#Loop pra manter a conexão com o servidor
 
+while True:
+
+    fim_transferencia = False
     requisicao = input("Insira o nome do arquivo a ser requisito no formato GET /nome_arquivo.ext ").strip().encode("utf-8")
     sock.sendto(requisicao,(udp_ip,udp_port))
 
-    while True:
+    lista_segmentos = []
+
+    #Loop para continuar recebendo dados até o arquivo estar completo
+    while not fim_transferencia:
         data,address = sock.recvfrom(1024) # recebe a informação (o 1024 é o tamanho do buffer)
 
-        print(data.decode())
+        data = data.decode("utf-8")
+        header,conteudo = data.split('|',maxsplit=1) 
+        
+        num_segmento, qtde_segmentos, checksum = header.split('#')
+        num_segmento, qtde_segmentos, checksum = int(num_segmento), int(qtde_segmentos), int(checksum) #O cabeçalho vem em string, reconverte-os pra int
+
+        auxChecksum = checksum_crc32(conteudo.encode())
+
+        print(f"Checksum Header:{checksum}; Checksum Aqui: {auxChecksum}")
+
+        #Se a condição abaixo for verdade, o segmento está corrompido, tem que pedir de novo
+        if(checksum != auxChecksum):
+            sock.sendto(f"RESEND /{num_segmento}", (udp_ip,udp_port))
+            
+
+    #TODO: AQUI FICARÁ A PARTE DE RECONSTRUÇÃO DO DOCUMENTO
+
+    
--- server.py
+++ server.py
@@ -15,8 +15,8 @@ Requisitos do Servidor UDP:
     FEITO    Verificar se o arquivo solicitado existe.
     FEITO       Se o arquivo não existir: Enviar uma mensagem de erro claramente definida pelo seu protocolo para o cliente.
     Transmissão do Arquivo (se existir):
-    TODO    Segmentação: Dividir o arquivo em múltiplos segmentos/pedaços para envio em datagramas UDP.
-    TODO    Cabeçalho Customizado: Cada segmento enviado deve conter informações de controle definidas pelo seu protocolo (ver “Considerações de Protocolo” abaixo).
+    FEITO    Segmentação: Dividir o arquivo em múltiplos segmentos/pedaços para envio em datagramas UDP.
+    FEITO    Cabeçalho Customizado: Cada segmento enviado deve conter informações de controle definidas pelo seu protocolo (ver “Considerações de Protocolo” abaixo).
     TODO    Retransmissão: Implementar lógica para reenviar segmentos específicos caso o cliente solicite (devido a perdas ou erros).
 
 
@@ -32,6 +32,7 @@ UDP_PORT = 5005
 
 TAM_BUFFER = 1024
 TAM_CABECALHO = 80 
+TAM_CONTEUDO = TAM_BUFFER - TAM_CABECALHO
 #80 pois foi verificado que, dependendo da quantidade de caracteres (seja pela quantidade de segmentos ou pelo tamanho do checksum)
 #o cabeçalho pesava cerca de 60 bytes (casos maiores, fora do escopo do trabalho, mas para garantir ficou assim). Foi deixado mais alguns para margem de segurança.
 
@@ -39,13 +40,13 @@ def checksum_crc32(segmento):
     return zlib.crc32(segmento) & 0xffffffff
 
 # Cria o objeto de tipo socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #AF_INET = ipv4 e SOCK_DGRAM = UDP
 sock.bind((UDP_IP,UDP_PORT)) #O servidor fica ouvindo nessa porta
 
 #Envia a mensagem no formato (mensagem (IP, PORT))só pra ter como base
 #sock.sendto(b"hello world", (UDP_IP, UDP_PORT))
 
-#Loop pra simular a conexão
+#Loop pra manter a conexão
 while True:
 
     data,address = sock.recvfrom(TAM_BUFFER) #Espera um novo cliente
@@ -62,32 +63,38 @@ while True:
 
         #Pega o arquivo se ele existir
         with open(nome_arquivo, "r") as arquivo:
-            conteudo = arquivo.read()
+            conteudo = arquivo.read().encode("utf-8")
 
         #Transmite o arquivo em pacotes diferentes
-        qtde_segmentos = math.ceil(len(conteudo/TAM_BUFFER))
+        qtde_segmentos = math.ceil(len(conteudo)/TAM_CONTEUDO)
 
         #Mensagem para fins de teste/debug
         print(f"Enviando o arquivo {nome_arquivo} para {address} em {qtde_segmentos} arquivos")
 
+        #Para caso precise reenviar TODO isso provavelmente vai dar errado já que o escopo desse e do resend são diferentes, arrumar isso
+        cache_segmentos = []
+
         #Envia cada segmentos do arquivo                
         for num_segmento in range(qtde_segmentos):
 
-            inicio_segmento = num_segmento * TAM_BUFFER - TAM_CABECALHO
-            fim_segmento = inicio_segmento + TAM_BUFFER - TAM_CABECALHO
+            inicio_segmento = num_segmento * TAM_CONTEUDO
+            fim_segmento = inicio_segmento + TAM_CONTEUDO
             bloco = conteudo[inicio_segmento:fim_segmento]
 
             checksum = checksum_crc32(bloco)
 
-            cabecalho = (f"{num_segmento}#{qtde_segmentos}#{checksum}|")
+            cabecalho = (f"{num_segmento}#{qtde_segmentos}#{checksum}|").encode("utf-8")
 
             segmento = cabecalho+bloco
+            cache_segmentos.append(segmento)
 
-            sock.sendto(segmento.encode("utf-8"), address)
+            sock.sendto(segmento, address)
+
+    elif(mensagem_cliente.startswith("RESEND /")):
+        #TODO: reenviar o segmento pedido
+        num_segmento_pedido = mensagem_cliente.split("/")[1]
+        sock.sendto(cache_segmentos[num_segmento_pedido], address)
 
     #Envia mensagem de erro caso o comando seja inválido (diferente de GET)
     else:
         sock.sendto(("ERRO: Comando inválido").encode("utf-8"), address) 
-        
-
-
