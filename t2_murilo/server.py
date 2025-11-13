e7d98de953eee602e97c744762c3c8b881a985cd
murilobf <murilobontorin2005@gmail.com>
2025-11-13 11:17:51 -0300

    Continuação da base dos protocolos e organização do código

--- t2_murilo/server.py
+++ t2_murilo/server.py
@@ -5,18 +5,31 @@ import os
 
 TAM_BUFFER = 4096
 
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-clientes_conectados = []
-clientes_lock = threading.Lock()
+#TODO: IMPLEMENTAR A VERIFICACAO DO SHA256
+def sha256(dado):
+    sha = hashlib.sha256()
 
 def broadcast(mensagem):
     
-    mensagem_formatada = f'SERVIDOR| {mensagem}'.encode()
+    mensagem_formatada = f'SERVIDOR|{mensagem}'.encode()
 
     with clientes_lock:
         for cliente in clientes_conectados:
-            cliente.sendall()#mesma coisa do send mas envia todos os segmentos
+            cliente.sendall(mensagem_formatada)#mesma coisa do send mas envia todos os segmentos
+
+def formata_tamanho(tamanho_bytes):
+    sufixos = ['B','kB','MB','GB']
+
+    for sufixo in sufixos:
+        formatado = str(tamanho_bytes) + sufixo
+
+        if(tamanho_bytes/1024 < 1):
+            break
+            
+        tamanho_bytes /= 1024
+
+    return round(formatado,2)
+
 
 def processar(conexao, endereco):
     #asd
@@ -34,7 +47,7 @@ def processar(conexao, endereco):
             dado = dado.decode()
 
             if dado.startswith("GET"):
-                addr, caminho_arquivo = dado.split("|")
+                _, caminho_arquivo = dado.split("|")
 
                 if not os.path.exists(caminho_arquivo):
                     msg_erro = f"ERRO 404|Arquivo solicitado nao encontrado. Caminho inserido: {caminho_arquivo}".encode()
@@ -44,18 +57,23 @@ def processar(conexao, endereco):
                 else:
                     with open(caminho_arquivo, 'r') as arquivo:
                         conteudo = arquivo.read().encode()
+                        tam_arquivo = formata_tamanho(len(conteudo))
+                        sha_arquivo = sha256(conteudo)
 
-                    conexao.sendall(conteudo) #TODO: tem que mandar 4KB por vez
+                        ack_inicio = f"INICIO|tamanho: {tam_arquivo}#hash sha256:{sha_arquivo}".encode()
+                        conexao.sendall(ack_inicio)
 
+                        conexao.sendall(conteudo) #TODO: tem que mandar 4KB por vez
+                    
             elif dado.startswith("CHAT"):
-                addr, mensagem = dado.split("|")
+                _, mensagem = dado.split("|")
 
-                print(f"MENSAGEM DE [{addr}]: {mensagem}")
+                print(f"MENSAGEM DE [{endereco}]: {mensagem}")
                 
             elif dado.startswith("SAIR"):
-                addr, mensagem = dado.split("|")
+                _, mensagem = dado.split("|")
 
-                print(f"SAIR: CLIENTE {addr} se desconectou")
+                print(f"SAIR: CLIENTE {endereco} se desconectou")
                 break
 
             else:
@@ -67,6 +85,14 @@ def processar(conexao, endereco):
         with clientes_lock:
             clientes_conectados.remove(conexao)
 
+#Cria servidor e binda na porta
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind(("0.0.0.0", 5000))
+servidor.listen(0)
+
+clientes_conectados = []
+clientes_lock = threading.Lock()
+
 #Aguarda clientes
 while True:
     print("\nAguardando novo cliente\n")
@@ -75,4 +101,3 @@ while True:
     print(f"Criando thread para cliente {conexao}")
     thread_cliente = threading.Thread(target=processar, args=(conexao,endereco))
     thread_cliente.start()
-
