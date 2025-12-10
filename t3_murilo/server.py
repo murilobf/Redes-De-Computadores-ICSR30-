3a51a03cc27719c273f03ea7ab859062d73b6bc2
Murilo <murilobontorin2005@gmail.com>
2025-12-10 14:44:47 -0300

    Trabalho 3 finalizado

--- t3_murilo/server.py
+++ t3_murilo/server.py
@@ -1,89 +1,121 @@
 import socket
 import threading
-import hashlib
 import os
 
 TAM_BUFFER = 4096
-
-def calcula_sha256(dado):
-    sha = hashlib.sha256()
-    sha.update(dado) 
-    return sha.hexdigest()
-
-def broadcast(mensagem):
-    
-    mensagem_formatada = f'BROADCAST|{mensagem}'.encode()
-
-    with clientes_lock:
-        for cliente in clientes_conectados:
-            cliente.sendall(mensagem_formatada)
-
-def processar_servidor():
-    while True:
-        mensagem = input("")
-        broadcast(mensagem)
+PORTA = 5006
 
 def processar_cliente(conexao, endereco):
     with clientes_lock:
         clientes_conectados.append(conexao)
 
     try:
-        while True:
-            dado = conexao.recv(TAM_BUFFER)
-            
-            if not dado:
-                print(f"Cliente {endereco} desconectado. Eliminando thread.")
-                break
+        dado = conexao.recv(TAM_BUFFER)
+        
+        if not dado:
+            print(f"Cliente {endereco} desconectado. Eliminando thread.")
 
-            dado = dado.decode()
+        dado = dado.decode()
 
-            try:
-                if dado.startswith("GET|"):
-                    _, caminho_arquivo = dado.split("|",1)
+        primeira_linha = dado.split('\n')[0]
+        print(primeira_linha)
+        requisicao = primeira_linha.split(' ')[0]
+        caminho_arquivo = primeira_linha.split(' ')[1]
+        caminho_arquivo = caminho_arquivo.split('/')[1]
+        if(caminho_arquivo == ''):
+                    caminho_arquivo = 'imagens.html'
 
-                    if not os.path.exists(caminho_arquivo):
-                        msg_erro = f"ERRO 404|Arquivo solicitado nao encontrado. Caminho inserido: {caminho_arquivo}".encode()
-                        conexao.sendall(msg_erro)
-                        continue
+        if(caminho_arquivo.endswith('.html')):
+            tipo_conteudo = 'text/html'
+        elif(caminho_arquivo.endswith('.jpeg') or caminho_arquivo.endswith('.jpg')):
+            tipo_conteudo = 'image/jpeg'
+        elif(caminho_arquivo.endswith('.png')):
+            tipo_conteudo = 'image/png'
 
-                    else:
-                        with open(caminho_arquivo, 'rb') as arquivo:
-                            conteudo = arquivo.read()
-                            tam_arquivo = (len(conteudo))
-                            sha_arquivo = calcula_sha256(conteudo)
+        try:
+            if requisicao.startswith("GET"): #Dá pra pegar a primeira linha da requisição http
+                print(f"Cliente: {endereco} solicitando: {caminho_arquivo}")
 
-                            msg_inicio = f"INICIO|{tam_arquivo}#{sha_arquivo}|".encode()
-                            conexao.sendall(msg_inicio)
+                if not os.path.exists(caminho_arquivo):
+                    corpo = (
+                        f"<html>"
+                        f"<head><title>404 Not Found</title></head>"
+                        f"<body><h1>404 Não encontrado</h1><p>O arquivo '{caminho_arquivo}' nao foi encontrado.</p></body>"
+                        f"</html>"
+                    ).encode()
 
-                            arquivo.seek(0)
-                            while True:
-                                bloco = arquivo.read(TAM_BUFFER)
-                                
-                                if not bloco:
-                                    break
+                    header = (
+                        "HTTP/1.1 404 Not Found\r\n"
+                        "Content-Type: text/html\r\n"
+                        f"Content-Length {len(corpo)}\r\n"
+                        "\r\n"
+                    ).encode()
 
-                                conexao.sendall(bloco)
-                        
-                elif dado.startswith("CHAT|"):
-                    _, mensagem = dado.split("|",1)
-
-                    print(f"MENSAGEM DE [{endereco}]: {mensagem}")
-                    msg_chat = b"CHAT|Mensagem recebida com sucesso!"
-                    conexao.sendall(msg_chat)
-                    
-                elif dado.startswith("SAIR"):
-                    print(f"SAIR: CLIENTE {endereco} se desconectou")
-                    break
+                    resposta = header+corpo
+                    conexao.sendall(resposta)
+                    conexao.close()
 
                 else:
-                    msg_erro = b"ERRO 400|Comando invalido"
+                    with open(caminho_arquivo, 'rb') as arquivo:
+                        conteudo = arquivo.read()
 
-                    conexao.sendall(msg_erro)
+                        tam_arquivo = (len(conteudo))
 
-            except Exception as e:
-                msg_erro = b"ERRO 400|Comando invalido"
+                        header = (
+                        f"HTTP/1.1 200 OK\r\n"
+                        f"Content-Type: {tipo_conteudo}\r\n"
+                        f"Content-Length: {tam_arquivo}\r\n"
+                        f"Connection: keep-alive\r\n"
+                        f"\r\n"
+                        ).encode()
+                        conexao.sendall(header)
 
-                conexao.sendall(msg_erro)
+                        
+                        arquivo.seek(0)
+                        while True:
+                            bloco = arquivo.read(TAM_BUFFER)
+                            if not bloco:
+                                break
+                            conexao.sendall(bloco)
+                    conexao.close()
+
+
+            else:
+                corpo_erro = (
+                    f"<html>"
+                    f"<head><title>400 Bad Request</title></head>"
+                    f"<body><h1>400 Bad Request</h1><p>Comando inválido.</p></body>"
+                    f"</html>"
+                ).encode()
+
+                header_erro = (
+                    "HTTP/1.1 400 Bad Request\r\n"
+                    "Content-Type: text/html\r\n"
+                    f"Content-Length: {len(corpo_erro)}\r\n"
+                    "\r\n"
+                ).encode()
+
+                conexao.sendall(header_erro + corpo_erro)
+
+
+        except Exception as e:
+            print(e)
+            corpo_erro = (
+                f"<html>"
+                f"<head><title>400 Bad Request</title></head>"
+                f"<body><h1>400 Bad Request</h1><p>Comando inválido.</p></body>"
+                f"</html>"
+            ).encode()
+
+            header_erro = (
+                "HTTP/1.1 400 Bad Request\r\n"
+                "Content-Type: text/html\r\n"
+                f"Content-Length: {len(corpo_erro)}\r\n"
+                "\r\n"
+            ).encode()
+
+            conexao.sendall(header_erro + corpo_erro)
+
             
     finally:
         with clientes_lock:
@@ -92,16 +124,13 @@ def processar_cliente(conexao, endereco):
                 conexao.close()
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind(("127.0.0.1", 5005))
+servidor.bind(("127.0.0.1", PORTA))
 servidor.listen(5)
 
 clientes_conectados = []
 clientes_lock = threading.Lock()
 
-thread_servidor = threading.Thread(target=processar_servidor)
-thread_servidor.start()
-
-print("SERVIDOR INICIALIZADO. Mensagens digitadas no terminal serão enviadas para todos os clientes conectados.")
+print(f"SERVIDOR INICIALIZADO no IP: http://127.0.0.1:{PORTA}/.")
 
 while True:
     conexao, endereco = servidor.accept()
